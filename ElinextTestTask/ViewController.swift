c8a40d5c2dc9917b19f6dec251c31f3fbcfac20a
Ivan Zavadsky <i.zavadsky@itransition.com>
2021-04-30 23:51:00 +0300

    Initial Commit

---
+++ ElinextTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElinextTestTask
+//
+//  Created by MacBook on 30.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
